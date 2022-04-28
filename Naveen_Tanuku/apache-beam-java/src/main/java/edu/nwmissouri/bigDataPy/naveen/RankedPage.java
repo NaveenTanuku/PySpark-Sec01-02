54a2d73af55dc8303f8cc0a9f7fe697f7808a94d
naveen <ntanuku96@gmail.com>
2022-04-27 23:35:28 -0500

    added some job 2 mapper

--- Naveen_Tanuku/apache-beam-java/src/main/java/edu/nwmissouri/bigDataPy/naveen/RankedPage.java
+++ Naveen_Tanuku/apache-beam-java/src/main/java/edu/nwmissouri/bigDataPy/naveen/RankedPage.java
@@ -1,13 +1,51 @@
 package edu.nwmissouri.bigDataPy.naveen;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RankedPage{
+public class RankedPage implements Serializable{
+    String voter;
+    double rank;
+    ArrayList<VotingPage> voterList = new ArrayList<>();
+
     public RankedPage(){
 
     }
 
-    public RankedPage(String s,  ArrayList<VotingPage> v){
+    public RankedPage(String voter,double rank, ArrayList<VotingPage> voterList){
+        this.voter = voter;
+        this.voterList = voterList;
+        this.rank = rank;
+    }    
 
+    public RankedPage(String voter,  ArrayList<VotingPage> voterList){
+        this.voter = voter;
+        this.voterList = voterList;
+        this.rank = 1.0;
+    }
+
+    public String getVoter() {
+        return voter;
+    }
+
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+
+    public ArrayList<VotingPage> getVoterList() {
+        return voterList;
+    }
+
+    public void setVoterList(ArrayList<VotingPage> voterList) {
+        this.voterList = voterList;
+    }
+
+    @Override
+    public String toString(){
+        return this.voter +"<"+ this.rank +","+ voterList +">";
+    }
+
+    public double getRank() {
+        return this.rank;
     }
 }
